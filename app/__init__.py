3364f3d5272d3aef8ab3e69e63e1f6fa6e09e743
Ikeh Ifeanyi <ikeifeanyi95.ii@gmail.com>
2022-05-09 20:13:46 -0700

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ app.config.from_object(app_settings)
 
 
 #connecting to database
-MONGO_URI = os.environ.get('MONGO_URI') or "mongodb+srv://fabulous95:Ifeanyi7.ii@cluster0.nz9zg.mongodb.net/HealthApp?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://fabulous95:Ifeanyi7.ii@cluster0.nz9zg.mongodb.net/HealthApp?retryWrites=true&w=majority"
 
 #initializing PyMongo
 mongo = PyMongo(app, MONGO_URI)
